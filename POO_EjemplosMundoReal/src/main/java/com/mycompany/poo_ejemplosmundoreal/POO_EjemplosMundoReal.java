a4bf2a7b1ede83969ba9916c338370fdd29a3773
lilipaqc <lp.quijijec@uea.edu.ec>
2025-06-21 21:29:48 -0500

    Primer commit: Proyecto Java inicial desde NetBeans

---
+++ POO_EjemplosMundoReal/src/main/java/com/mycompany/poo_ejemplosmundoreal/POO_EjemplosMundoReal.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.poo_ejemplosmundoreal;
+
+/**
+ *
+ * @author USER
+ */
+public class POO_EjemplosMundoReal {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
